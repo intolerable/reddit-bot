90bdc57a54cdb78db93ada65ca18c0835e5deb42
Fraser Murray <fraser.m.murray@gmail.com>
2014-05-10 18:00:19 +0100

    moved post detection to its own module

---
+++ src/Classifier.hs
@@ -0,0 +1,56 @@
+module Classifier where
+
+import Parser
+
+import Data.Char (isAlpha, toLower)
+import Data.List (genericLength, isPrefixOf)
+import Data.Monoid
+import Data.Text (Text)
+import Reddit.API.Types.Post (Post, PostContent(..))
+import Text.Parsec (parse)
+import qualified Data.Map as Map
+import qualified Data.Text as Text
+import qualified Reddit.API.Types.Post as Post
+
+data PostClassification = Unrelated
+                        | NewPlayer
+                        | LoLSwitch
+  deriving (Show, Read, Eq)
+
+getWordList :: IO (Maybe WordList)
+getWordList = do
+  let filename = "words.list"
+  f <- readFile filename
+  case parse wordList filename $ Text.pack f of 
+    Left _ -> return $ Nothing
+    Right x -> return $ Just x
+
+keywords :: Text -> [Text]
+keywords t = filter (not . Text.null) . Text.split (not . isAlpha) . Text.map toLower $ t 
+
+shouldBotRespond :: WordList -> Post -> Bool
+shouldBotRespond w post = score w post > 0.25 && hasEnoughWords post
+
+hasEnoughWords :: Post -> Bool
+hasEnoughWords p = length (keywords $ postTextTitle p) > 15
+
+score :: WordList -> Post -> Double
+score w post = helper $ postTextTitle post
+  where helper t = (total w (keywords t)) / genericLength (keywords t)
+
+postTextTitle :: Post -> Text
+postTextTitle p =
+  Post.title p <> " " <> case Post.content p of
+    SelfPost s _ -> s
+    Link _ -> ""
+    TitleOnly -> ""
+
+total :: WordList -> [Text] -> Double
+total w ts = fromIntegral . sum $ map (\(ws, value) -> (ws `countInstancesIn` ts) * value) (Map.toList w)  
+
+countInstancesIn :: Eq a => [a] -> [a] -> Integer
+countInstancesIn [] _ = 0
+countInstancesIn _ [] = 0
+countInstancesIn x y = (f $ x `isPrefixOf` y) + countInstancesIn x (tail y)
+  where f False = 0
+        f True = 1
