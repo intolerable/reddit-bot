ef6ab8c6b723f5f4475e3edad41959b8927d192c
Fraser Murray <fraser.m.murray@gmail.com>
2014-05-18 14:04:41 +0100

    fixed incorrect examples and added more

--- src/Examples.hs
+++ src/Examples.hs
@@ -16,10 +16,10 @@ examplesCorrect :: Reddit [Post.Post]
 examplesCorrect = mapM Reddit.getPostInfo exampleIDsCorrect
 
 exampleIDsCorrect :: [Post.PostID]
-exampleIDsCorrect = map Post.PostID ["24jh8m", "24eme3", "24emhg", "24nsxb", "24opbh", "24o8sh", "24v66k", "24vt4p", "25audt", "25j3qm"]
+exampleIDsCorrect = map Post.PostID ["24jh8m", "24eme3", "24emhg", "24nsxb", "24opbh", "24o8sh", "24v66k", "24vt4p", "25j3qm", "25le2f", "25jzxx", "25ot8f", "25r8cr", "25sdb5", "25tfu1", "25uxdk"]
 
 examplesIncorrect :: Reddit [Post.Post]
 examplesIncorrect = mapM Reddit.getPostInfo exampleIDsIncorrect
 
 exampleIDsIncorrect :: [Post.PostID]
-exampleIDsIncorrect = map Post.PostID ["24kqrz", "24kpwq", "24kokp", "24lci7", "24liv1", "24ot78", "24ohpk", "24pban", "24q7t5", "24r432", "24rzmo", "24seij", "24slxo", "24u3ti", "24vgan", "24vt9t", "257752", "2570tb", "256tak", "256gi7", "2581ac", "25816s", "25812f", "2580vf", "2580w8", "2581ds", "25aij5", "25a9ev", "25d8k5", "25h1ed", "25honv", "25hvaj", "25jeyr"]
+exampleIDsIncorrect = map Post.PostID ["24kqrz", "24kpwq", "24kokp", "24lci7", "24liv1", "24ot78", "24ohpk", "24pban", "24q7t5", "24r432", "24rzmo", "24slxo", "24u3ti", "24vgan", "24vt9t", "257752", "2570tb", "256tak", "256gi7", "2581ac", "25816s", "25812f", "2580vf", "2580w8", "2581ds", "25aij5", "25a9ev", "25d8k5", "25h1ed", "25honv", "25hvaj", "25jeyr", "25ku8x", "25m7lk", "25o1ak", "25odgj", "25pc9p", "25q8f9", "25sjmt", "25rbw9", "25r2yv", "25shyl", "25t3ni", "25uql5", "25audt", "25uveq"]
